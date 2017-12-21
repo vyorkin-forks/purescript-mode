6463a9abdd921a47750b9534d34949f2c47897f3
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2017-12-21 19:39:30 +0300

    Disable quail rule for <= char sequence

--- purescript-unicode-input-method.el
+++ purescript-unicode-input-method.el
@@ -225,7 +225,7 @@ packages (base-unicode-symbols and containers-unicode-symbols).
  ;; Relational operators
  ("=="               ["≡"])
  ("/="               ["≢" "≠"])
- ("<="               ["≤"])
+ ; ("<="               ["≤"])
  (">="               ["≥"])
  ("/<"               ["≮"])
  ("/>"               ["≯"])
